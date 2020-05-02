a07c831d040f77f0db05e642ebefc6b88d524868
Angeljot <62397929+Angeljot@users.noreply.github.com>
2020-05-02 20:04:22 +0530

    Update music_player_using_python.py

--- music_player_using_python.py
+++ music_player_using_python.py
@@ -1,16 +1,16 @@
 from pygame import mixer
-mixer.init()
-mixer.music.load("E:/New folder (4)/Songs/Pehla Pyaar.mp3")
-mixer.music.set_volume(1.0)
-mixer.music.play(0)
+mixer.init() #starting the music
+mixer.music.load("E:/New folder (4)/Songs/Pehla Pyaar.mp3") #loading music
+mixer.music.set_volume(1.0) #setting volume in range 0.0 to 1.0
+mixer.music.play(0) #0 for one play and -1 for infinite play
 while True:
     print("Press 'p' to pause, 'r' to resume")
     print("Press 'e'to exit")
     query=input(">>> ")
     if query=='p':
-        mixer.music.pause()
+        mixer.music.pause() #to pause music
     elif query=='r':
-        mixer.music.unpause()
+        mixer.music.unpause()#to resume music
     elif query=='e':
-        mixer.music.stop()
+        mixer.music.stop() #to stop music
         break
